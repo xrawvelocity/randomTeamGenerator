417b157d256a4de1e0447aa2758125b15cce8715
Victor Fernandez <vmfs99@gmail.com>
2020-01-28 16:49:22 -0500

    improved dom a bit

--- src/script.js
+++ src/script.js
@@ -1,43 +1,21 @@
 /*jshint esversion: 9*/
-// const teamGen = (array, num) => {
-//     let index = array.length;
-//     let temporary, random;
-//     let teams = {};
-//     while(index){
-//       random = Math.floor(Math.random() * index);
-//       index -= 1;
-  
-//       temporary = array[index];
-//       array[index] = array[random];
-//       array[random] = temporary;
-//     }
-//     for(let x = 0; x < array.length / num; x++){
-//       teams[`teams${x + 1}`] = [];
-//     }
-//     for(let i = 0; i < array.length; i += num){
-//       // teams['team' + i] = [].concat( array[i], array[i+num]);
-//       // array.slice(0, i+=num)
-//       for(let team in teams){
-//         teams[team].push(array.slice(i, num));
-//       }
-//    }
-
-    
-//     console .log(teams);
-//     result.innerHTML = `${names}`;
-//   };
   
   let names = [];
   let noperTeam;
+  let round = 0;
+  let bigArray = [];
+
   let form = document.getElementById('team-form');
   let result = document.querySelector('.result');
+  let table = document.querySelector('.table');
 
   form.onsubmit = e => {
     e.preventDefault();
     names = [...form.teamNames.value.split(', ')];
     noPerTeam = form.teamNumbers.value;
 
-    let round = 0;
+    
+  
 
     if (names){
       if (names.length % noPerTeam === 0){
@@ -49,13 +27,37 @@
 
     let noOfTeams = Math.floor(round);
     var totalTeams = (names.length/noOfTeams);  
-    
+
     var i,j,temparray,piece = totalTeams;
-    for (i = 0, j= names.length; i<=j; i+=piece) {
+    for (i = 0, j= names.length; i<j; i+=piece) {
         temparray = names.slice(i,i+piece);
-        console.log(temparray);
+        bigArray.push(temparray);
     }
-    console.log(noOfTeams);
-    console.log(round);
+    let html;
+    table.parentElement.removeChild(table);
+
+    for(i = 0; i < bigArray.length; i++){
+    html = `
+    <table class="table">
+      <thead><th>Team %num%</th></thead>
+      ${'<tr><td>%name%</td></tr>'.repeat(bigArray[i].length)}
+    </table>
+    `;
+
+    let newhtml = html.replace('%num%', '2');
+    newhtml = newhtml.replace('%name%', bigArray[0][1]);
+    result.insertAdjacentHTML("beforeend", newhtml);
+    }
+    console.log(html);
+
+  
+    
+    
+    console.log(bigArray);
   
   };
+
+
+
+
+
