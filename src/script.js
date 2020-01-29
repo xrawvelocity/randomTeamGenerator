d3424a322fbb88126d913094dcf108dc5a46fe0e
Victor Fernandez <vmfs99@gmail.com>
2020-01-28 19:08:35 -0500

    added reset button & fixed delete bug

--- src/script.js
+++ src/script.js
@@ -8,16 +8,23 @@
   let bigArray = [];
 
   /* DOM VARIABLES */
-  let form = document.getElementById('team-form');
+  let form = document.getElementById('team-form--2');
+  let reset = document.querySelector('.reset');
   let result = document.querySelector('.result');
   let table = document.querySelectorAll('.table');
+  let heading = document.querySelector('.heading-1');
+  console.log(table);
 
-
-  //when the button is clicked do this
+  //when the 'make teams' button is clicked do this
   form.onsubmit = e => {
 
     //dont clear values of inputs
     e.preventDefault();
+
+    //margin: 40rem auto 5rem auto;
+    //make the space on top smaller on click
+    heading.style.margin = '10rem auto 5rem auto';
+
 
     //get input into variables
     namesArray = [...form.teamNames.value.split(', ')];
@@ -28,14 +35,11 @@
 
     var totalTeams = (namesArray.length/numOfTeams);  
 
-    var i,j,temparray,piece = totalTeams;
+    var temparray,piece = totalTeams;
     for (i = 0, j= namesArray.length; i<j; i+=piece) {
         temparray = namesArray.slice(i,i+piece);
         bigArray.push(temparray);
     }
-    //if(table) table.parentNode.removeChild(table);
-
-    table.forEach(e => e.parentNode.removeChild(e));
 
     for(i = 0; i < bigArray.length; i++){
     html = `
@@ -43,7 +47,7 @@
       <thead><th>Team %num%</th></thead>
       ${'<tr><td>%name%</td></tr>'.repeat(3)}
     </table>
-    `.repeat(3);
+    `.repeat(numOfTeams);
 
     let newhtml = html.replace('%num%', i);
     newhtml = newhtml.replace('%name%', bigArray[0][1]);
@@ -53,13 +57,18 @@
 
 
     /* TESTING */
-    console.log(html);
+    //console.log(html);
     
-    console.log(bigArray);
+    //console.log(bigArray);
   
   };
 
 
+/* when the 'reset' button is clicked do this */
 
-
+reset.onclick = e => {
+  result.innerHTML = '';
+  heading.style.margin = '40rem auto 5rem auto';
+  console.log('working');
+};
 
